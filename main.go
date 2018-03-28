18a86a031dda4181849fc6aa74801e52a34a6e87
ben <smoilbig@126.com>
2018-03-28 22:39:34 +0800

    ...

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 /**
  * Mass Texting
+ * 1.读取数据库，取出电话号码列表。查询未发的例表。
+ * 2.计时发消息。发送一条，标记一条。成功与否也记录下来。
+ * 连接数据库，查询数据。取到数据。记录到数组中，按顺序取值，并操作
+ *
  */
 package main
 
 import "fmt"
+
+
+func mysqlDBConnection()  {
+
+}
+
+func getPhoneList()  {
+
+}
+
+func postSMS() {
+
+
+}
+
+
+
+
+
 
 func main()  {
 	fmt.Println("Mass Texting!")
